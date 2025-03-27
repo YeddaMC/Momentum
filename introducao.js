2874902f541c6f64503dd0c2ed245d637d0dbee6
YeddaMC <160504359+YeddaMC@users.noreply.github.com>
2025-03-27 14:55:57 -0300

    Update introducao.js
    ...

--- introducao.js
+++ introducao.js
@@ -1,1 +1,10 @@
+// Aguarda o carregamento completo da página
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle"); // Seleciona o botão de menu
+    const navLinks = document.querySelector(".nav-links"); // Seleciona a lista de links do menu
 
+    // Adiciona um evento de clique ao botão do menu
+    menuToggle.addEventListener("click", function () {
+        navLinks.classList.toggle("active"); // Adiciona ou remove a classe "active"
+    });
+});
